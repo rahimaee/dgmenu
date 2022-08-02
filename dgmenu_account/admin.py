10805c551d0f5af1a9c66f7adceeb11533cc79e5
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-02 19:48:48 +0430

    CustomUsers add to admin.py

--- dgmenu_account/admin.py
+++ dgmenu_account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ['pk', 'email', 'username', 'first_name', 'last_name', 'PhoneNumber', 'Profile', 'Gender']
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'PhoneNumber', 'Profile', 'Gender')}),
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('PhoneNumber', 'Profile', 'Gender')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
