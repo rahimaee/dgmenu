69db5b71ba218da25a3be88c033565585d7da513
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-09 04:30:46 +0430

    Most of the features are written but not complete

--- dgmenu_cafe/models.py
+++ dgmenu_cafe/models.py
@@ -22,17 +22,17 @@ def upload_image_path(instance, filename):
 
 class Cafe(models.Model):
     Manager = models.ForeignKey(User, models.CASCADE, verbose_name='مدیر کافه')
-    Cafe_Name = models.CharField(max_length=150, verbose_name='', null=True, blank=True)
-    Cafe_UserName = models.CharField(max_length=150, verbose_name='', null=True, blank=False)
-    Cafe_Description = models.CharField(max_length=250, verbose_name='', null=True, blank=True)
-    Cafe_keywords = models.CharField(max_length=250, verbose_name='', null=True, blank=True)
-    Cafe_Address = models.CharField(max_length=250, verbose_name='', null=True, blank=True)
-    Cafe_Tel1 = models.CharField(max_length=100, verbose_name='', null=True, blank=True)
-    Cafe_Tel2 = models.CharField(max_length=100, verbose_name='', null=True, blank=True)
-    Cafe_Tel3 = models.CharField(max_length=100, verbose_name='', null=True, blank=True)
-    Cafe_Email = models.CharField(max_length=200, verbose_name='', null=True, blank=True)
-    Cafe_Instagram = models.CharField(max_length=200, verbose_name='', null=True, blank=True)
-    Cafe_Opening_Hours = models.CharField(max_length=250, verbose_name='', null=True, blank=True)
+    Cafe_Name = models.CharField(max_length=150, verbose_name='نام کافه', null=True, blank=True)
+    Cafe_UserName = models.CharField(max_length=150, verbose_name='نام کاربری کافه ', null=True, blank=False)
+    Cafe_Description = models.CharField(max_length=250, verbose_name='توضیح کافه', null=True, blank=True)
+    Cafe_keywords = models.CharField(max_length=250, verbose_name='کلمات کیدی', null=True, blank=True)
+    Cafe_Address = models.CharField(max_length=250, verbose_name='ادرس کافه', null=True, blank=True)
+    Cafe_Tel1 = models.CharField(max_length=100, verbose_name='شماره تماس1', null=True, blank=True)
+    Cafe_Tel2 = models.CharField(max_length=100, verbose_name='شماره تماس 2', null=True, blank=True)
+    Cafe_Tel3 = models.CharField(max_length=100, verbose_name='شماره تماس 3', null=True, blank=True)
+    Cafe_Email = models.CharField(max_length=200, verbose_name='ادرس ایمیل', null=True, blank=True)
+    Cafe_Instagram = models.CharField(max_length=200, verbose_name='اینستاگرام', null=True, blank=True)
+    Cafe_Opening_Hours = models.CharField(max_length=250, verbose_name='ساعت کاری', null=True, blank=True)
     Cafe_Header_Logo = models.ImageField(upload_to=upload_image_path, null=True, blank=True,
                                          verbose_name='Header_Logo')
     Cafe_favicon = models.ImageField(upload_to=upload_image_path, null=True, blank=True,
@@ -43,7 +43,7 @@ class Cafe(models.Model):
                                                  verbose_name='Icon120')
     Cafe_Apple_Touch_Icon76 = models.ImageField(upload_to=upload_image_path, null=True, blank=True,
                                                 verbose_name='Icon76')
-    Admin_Is_Active = models.BooleanField(default=False, verbose_name='')
-    Is_Active = models.BooleanField(default=False, verbose_name='')
+    Admin_Is_Active = models.BooleanField(default=False, verbose_name='فعال/غیرفعال مدیرت')
+    Is_Active = models.BooleanField(default=False, verbose_name='فعال/غیرفعال کاربر')
     Submit_Time = models.DateTimeField()
     Last_Edit_Time = models.DateTimeField()
