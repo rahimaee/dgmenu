683015e16e27bd8a37bc190a206133df7a837049
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-09 04:43:53 +0430

    make dgmenu_food_Category startapp fix C to c

--- dgmenu_food_category/apps.py
+++ dgmenu_food_category/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DgmenuFoodCategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dgmenu_food_Category'
+    name = 'dgmenu_food_category'
