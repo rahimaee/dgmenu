8bd097eecb71c8430c9354a1793ac9e4ade09180
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-10 05:56:16 +0430

    fix test url

--- dgmenu_cafe/urls.py
+++ dgmenu_cafe/urls.py
@@ -4,5 +4,6 @@ from .views import cafe_home_page
 
 app_name = 'dgmenu_cafe'
 urlpatterns = [
-    path('<cafename>/<s>', cafe_home_page),
+    path('<cafename>/', cafe_home_page),
+    path('<cafename>/<ss>', cafe_home_page),
 ]
