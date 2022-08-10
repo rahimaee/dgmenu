5c297ddb5a640a2d2fbff96fa2a21bcd2758ec1e
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-10 05:39:12 +0430

    add one fuc to manage view for cafe_home_page.html

--- dgmenu_cafe/views.py
+++ dgmenu_cafe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def cafe_home_page(request, *args, **kwargs):
+    ct = {}
+    print(request.path)
+    print(kwargs['cafename'])
+    return render(request=request, template_name='dgmenu_cafe/cafe_home_page.html', context=ct)
