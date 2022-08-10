618e85a39622578820553a8e8fb9cae36276bb1d
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-10 05:35:47 +0430

    admin.py add model to admin panel

--- dgmenu_food_category/admin.py
+++ dgmenu_food_category/admin.py
@@ -1,3 +1,10 @@
+# from django.contrib import admin
+# from .models import FoodCategory
+#
+# # Register your models here.
+# admin.site.register(FoodCategory)
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import FoodCategory
 
-# Register your models here.
+admin.site.register(FoodCategory, MPTTModelAdmin)
