bb21d9292be77c929b316368b02257b985090aef
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-26 15:42:31 +0430

    add def cafe_about for views.py

--- dgmenu_cafe_about/views.py
+++ dgmenu_cafe_about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def cafe_about(request, *args, **kwargs):
+    cx = {'CafeUserId': 12}
+    return render(request, 'dgmenu_cafe_about/cafe_about_page.html', cx)
