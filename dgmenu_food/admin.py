b45a74ee5373ceba9ca310131e64dd3fab4845d4
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-26 15:24:41 +0430

    اضافه کردن گالری تصاویر در ادمین پنل برای غذا

--- dgmenu_food/admin.py
+++ dgmenu_food/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Gallery
+
+
+class FoodGalleryInline(admin.StackedInline):
+    model = Gallery
+
+
+class FoodAdmin(admin.ModelAdmin):
+    inlines = [FoodGalleryInline]
+
 
 # Register your models here.
-admin.site.register(Food)
+admin.site.register(Food, FoodAdmin)
