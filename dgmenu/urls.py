0e60ac5385a7c433854df462d8b37bd4eb958fcf
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-26 15:27:56 +0430

    اضافه شدن url تیم کافه

--- dgmenu/urls.py
+++ dgmenu/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,9 +21,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from dgmenu_cafe.views import partial_view, header_partial_view, footer_partial_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dgmenu_cafe.urls', namespace='dgmenu_cafe')),
+    path('', include('dgmenu_cafe_team.urls', namespace='dgmenu_cafe_team')),
+    url(r'^partial-view/(?P<CafeUserId>\w+)$',
+        partial_view,
+        name='partial_view'),
+    url(r'^header_partial-view/(?P<CafeUserId>\w+)$',
+        header_partial_view,
+        name='header_partial_view'),
+    url(r'^footer_partial-view/(?P<CafeUserId>\w+)$',
+        footer_partial_view,
+        name='footer_partial_view'),
 ]
 if settings.DEBUG:
     # add root static files
