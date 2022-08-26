f76a728be7222506e1375feb4d38a0147d75ea29
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-26 15:28:50 +0430

    تغییر کلی

--- dgmenu_cafe/urls.py
+++ dgmenu_cafe/urls.py
@@ -1,9 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 
-from .views import cafe_home_page
+from .views import cafe_home_page, partial_view
 
 app_name = 'dgmenu_cafe'
 urlpatterns = [
     path('<cafename>/', cafe_home_page),
-    path('<cafename>/<ss>', cafe_home_page),
+
 ]
