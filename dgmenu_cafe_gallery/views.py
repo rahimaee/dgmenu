f9ec8707a08e7bd6e9d42182162b30bbdc321873
mohammad reza <mohammad.rahimaee@gmail.com>
2022-08-27 16:00:14 +0430

    add def cafe_gallery_page

--- dgmenu_cafe_gallery/views.py
+++ dgmenu_cafe_gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def cafe_gallery_page(request, *args, **kwargs):
+    cx = {
+        'CafeUserId': 12
+    }
+    return render(request, 'dgmenu_cafe_gallery/cafe_gallery_page.html', cx)
