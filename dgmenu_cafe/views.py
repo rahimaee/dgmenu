45aeb3066c16d1c0b6da0d838c95061b32bbabb2
mohammad reza <mohammad.rahimaee@gmail.com>
2022-09-14 00:26:02 +0430

    add footer logo views

--- dgmenu_cafe/views.py
+++ dgmenu_cafe/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
+from dgmenu_cafe.models import Cafe
 
 
 # Create your views here.
 
 def cafe_home_page(request, *args, **kwargs):
-    cx = {'CafeUserId': 12}
-    print(request.path)
+    cafe_name = str(request.path).split('/')[1]
+    cafe = Cafe.objects.filter(Cafe_UserName=cafe_name).first()
+    if cafe is None:
+        raise Http404("کافه پیدا نشد")
+    if cafe.Is_Active is True:
+        raise Http404("کافه غیرفعال می باشد")
+    cx = {'cafe': cafe,
+          'CafeUserId': cafe.id}
     return render(request=request, template_name='dgmenu_cafe/cafe_home_page.html', context=cx)
 
 
@@ -28,6 +35,13 @@ def header_partial_view(request, *args, **kwargs):
 
 
 def footer_partial_view(request, *args, **kwargs):
-    result = kwargs['CafeUserId']
-    kwargs['result'] = result
-    return render(request, 'shared/cafe/_Footer.html', kwargs)
+    CafeUserId = kwargs['CafeUserId']
+    cafe = Cafe.objects.filter(id=CafeUserId).first()
+    if cafe is None:
+        raise Http404("کافه پیدا نشد")
+    if cafe.Is_Active is True:
+        raise Http404("کافه غیرفعال می باشد")
+    cx = {'cafe': cafe,
+          'CafeUserId': cafe.id}
+
+    return render(request, 'shared/cafe/_Footer.html', cx)
--- dgmenu_food_category/models.py
+++ dgmenu_food_category/models.py
@@ -5,10 +5,10 @@ from dgmenu_cafe.models import Cafe
 # Create your models here.
 
 class FoodCategory(models.Model):
-    Cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE, verbose_name='', )
+    Cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE, verbose_name='')
     NameFa = models.CharField(max_length=120, verbose_name='')
     NameEn = models.CharField(max_length=120, verbose_name='')
     IsActive = models.BooleanField(default=True, verbose_name='فعال/غیرفعال')
     IsActiveAdmin = models.BooleanField(default=True, verbose_name='فعال/غیرفعال')
-    Parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+    Parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     First = models.IntegerField()
