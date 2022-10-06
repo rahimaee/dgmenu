7d6d77849a0da3df6308379ea96cc91b2e98427b
mohammad reza <mohammad.rahimaee@gmail.com>
2022-10-06 05:11:48 +0330

    fix

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request, *args, **kwargs):
+    cx = {}
+    return render(request, 'adminpanel/adminpanel_home_page.html', cx)
