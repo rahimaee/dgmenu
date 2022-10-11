0cf4d0c698b6eac8a0cae6cb1be4dbb991c69379
mohammad reza <mohammad.rahimaee@gmail.com>
2022-10-11 20:41:15 +0330

    fix partial view

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from dgmenu_account.models import CustomUser as User
 
 
 # Create your views here.
@@ -6,3 +7,26 @@ from django.shortcuts import render
 def index(request, *args, **kwargs):
     cx = {}
     return render(request, 'adminpanel/adminpanel_home_page.html', cx)
+
+
+def header_references_partial_view(request, *args, **kwargs):
+    cx = {}
+    return render(request, 'shared/panel/_HeaderReferences.html', cx)
+
+
+def header_panel_partial_view(request, *args, **kwargs):
+    cx = {}
+    return render(request, 'shared/panel/_Header.html', cx)
+
+
+def quick_bar_panel_partial_view(request, *args, **kwargs):
+    cx = {}
+    return render(request, 'shared/panel/_QuickBar.html', cx)
+
+
+def navbar_panel_partial_view(request, *args, **kwargs):
+    userid = request.user
+    user = User.objects.filter(id=userid.id).first()
+
+    cx = {'user': user}
+    return render(request, 'shared/panel/_Navbar.html', cx)
